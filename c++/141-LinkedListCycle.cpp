936554fc5b0c554a7e85ac5c9c09c616d9360f13
keen-learner <keen-learner@github.com>
2024-02-07 20:14:33 -0800

    add 206 Reverse Linked List

--- c++/141-LinkedListCycle.cpp
+++ c++/141-LinkedListCycle.cpp
@@ -28,6 +28,17 @@ Constraints:
 Follow up: Can you solve it using O(1) (i.e. constant) memory?
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
